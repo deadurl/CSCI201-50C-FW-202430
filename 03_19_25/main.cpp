c681b3349cdfdf025112d3df61d867e1dab53b43
Angela Venable <avenable11@ivytech.edu>
2025-03-19 19:34:35 -0400

    scope

--- 03_19_25/main.cpp
+++ 03_19_25/main.cpp
@@ -13,6 +13,20 @@ int main()
     myClock.setTime(2, 2, 2);
     myClock.printTime();
     myClock.equalTime(yourClock);
+    yourClock.printTime();
+    yourClock = myClock;
+    yourClock.printTime();
+    if (true)
+    {
+        x = 7;
+        int y = 12;
+    }
+    y = 122;
 
     return 0;
+}
+
+void myFunc()
+{
+    x = 7;
 }
